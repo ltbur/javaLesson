258f0b63247ea7747c0b23530b00a593799647fd
ltbur <ltburtonst@gmail.com>
2022-03-17 13:17:58 +0900

    乱数の生成

--- day3/Code2_14.java
+++ day3/Code2_14.java
@@ -1,6 +1,7 @@
+import java.util.*;
 public class Code2_14{
 	public static void main(String[] args){
-		int r = new java.util.Random().nextInt(90);
+		int r = new Random().nextInt(90);
 		System.out.println("あなたはたぶん"
 				+ r +"歳ですね");
 	}
