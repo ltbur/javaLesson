62446fde0467da0a2e2f94c8f246cb580d16a314
ltbur <ltburtonst@gmail.com>
2022-03-17 11:04:32 +0900

    Rpg.java追加

---
+++ day2/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+	public static void main(String[] args){
+		int money;
+		money = 30000;
+		System.out.println("予算:" + money);
+		System.out.println("金額:" + money);
+		money = money - 14800;
+		System.out.println("所持金" + money);
+	}
+}
